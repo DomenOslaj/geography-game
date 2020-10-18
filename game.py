6f32e83e31b10ed3ae3f6d7ec55a4120f9420612
Domen Oslaj <doslaj@gmail.com>
2020-10-18 23:11:12 +0200

    program select random state

--- game.py
+++ game.py
@@ -1,2 +1,9 @@
 # A local gaming company contacted you to build a game for them. It is a simple geography quiz where a user has to guess the capital city of some country
+import random
 
+capital_city_dict = {"Austria": "Vienna", "Slovenia": "Ljubljana", "Italy": "Rome", "Germany": "Berlin",
+                     "Croatia": "Zagreb", "Hungary": "Budapest"}
+
+random_state = random.choice(list(capital_city_dict))
+
+print(random_state)
