1b7bb61130f0214f8ab1dc73456e06196176f96b
Domen Oslaj <doslaj@gmail.com>
2020-10-18 23:32:38 +0200

    while loop for user guesses

--- game.py
+++ game.py
@@ -6,4 +6,18 @@ capital_city_dict = {"Austria": "Vienna", "Slovenia": "Ljubljana", "Italy": "Rom
 
 random_state = random.choice(list(capital_city_dict))
 
-print(random_state)
+guess = input("Game: What is the capital city of {0}? \n".format(random_state))
+print("User: {0}". format(guess))
+
+while True:
+    if capital_city_dict[random_state].lower() == guess.lower():
+        print("Game: You are correct!")
+        break
+    elif capital_city_dict[random_state].lower() != guess.lower():
+        print("Game: Wrong, try again. ")
+        another_guess = input("Game: Another guess: ")
+        if another_guess.lower() == capital_city_dict[random_state].lower:
+            print("Game: Correct!")
+            break
+    else:
+        print("Game: I dont understand you, try again.")
