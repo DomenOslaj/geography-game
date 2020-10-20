d8daaf540f68080126a3bea6bdfc7c28ef624d90
Domen Oslaj <doslaj@gmail.com>
2020-10-20 13:59:32 +0200

    def play_game and play_game added, opening selection

--- game.py
+++ game.py
@@ -1,23 +1,38 @@
-# A local gaming company contacted you to build a game for them. It is a simple geography quiz where a user has to guess the capital city of some country
+# A local gaming company contacted you to build a game for them. It is a simple geography quiz where a user has to
+# guess the capital city of some country
 import random
 
 capital_city_dict = {"Austria": "Vienna", "Slovenia": "Ljubljana", "Italy": "Rome", "Germany": "Berlin",
                      "Croatia": "Zagreb", "Hungary": "Budapest"}
 
+# chose random state
 random_state = random.choice(list(capital_city_dict))
 
-guess = input("Game: What is the capital city of {0}? \n".format(random_state))
-print("User: {0}". format(guess))
 
-while True:
-    if capital_city_dict[random_state].lower() == guess.lower():
-        print("Game: You are correct!")
-        break
-    elif capital_city_dict[random_state].lower() != guess.lower():
-        print("Game: Wrong, try again. ")
-        another_guess = input("Game: Another guess: ")
-        if another_guess.lower() == capital_city_dict[random_state].lower:
-            print("Game: Correct!")
+# ask user for  capital city of that state
+def user_guess():
+    guess = input("Game: What is the capital city of {0}? \n".format(random_state))
+    print("User: {0}". format(guess))
+    return guess.lower()
+
+
+# play game
+def play_game(guess):
+    while True:
+        if capital_city_dict[random_state].lower() == guess():
+            print("Game: You are correct!")
             break
+        else:
+            print("Game: I don`t understand you, try again.")
+
+
+# opening selection
+while True:
+    selection = input("Would you like to A) play a game, B) exit: ")
+    if selection.lower() == "a":
+        play_game(user_guess)
+    elif selection.lower() == "b":
+        break
     else:
-        print("Game: I dont understand you, try again.")
+        print("Only A) and B) possibilities to choose from!")
+
